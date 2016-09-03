c754fea57912889f7f8928b93ffc8daca9069fe8
zouhairireda <zouhairireda@gmail.com>
2016-09-03 23:50:18 +0200

    first commit for spring boot poc

---
+++ src/main/java/pocs/runner/SpringWebApplication.java
@@ -0,0 +1,12 @@
+package pocs.runner;
+
+import org.springframework.boot.SpringApplication;
+
+import pocs.rest.Example;
+
+public class SpringWebApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Example.class, args);
+	}
+}
